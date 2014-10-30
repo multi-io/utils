bd3cc9083067420d7211bb26425d6aee0897f59c
Olaf Klischat <olaf.klischat@gmail.com>
2014-10-30 16:46:44 +0100

    Linux/file-cached-blocks.c: better reporting

--- Linux/file-cached-blocks.c
+++ Linux/file-cached-blocks.c
@@ -1,4 +1,4 @@
-// from http://insights.oetiker.ch/linux/fadvise/
+// adapted from http://insights.oetiker.ch/linux/fadvise/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,12 +22,15 @@ int main(int argc, char *argv[]) {
     mincore_vec = calloc(1, (file_stat.st_size+page_size-1)/page_size);
     mincore(file_mmap, file_stat.st_size, mincore_vec);
     printf("Cached Blocks of %s: ",argv[1]);
-    for (page_index = 0; page_index <= file_stat.st_size/page_size; page_index++) {
+    unsigned n_pages = 1 + file_stat.st_size/page_size;
+    unsigned n_cached = 0;
+    for (page_index = 0; page_index < n_pages; page_index++) {
         if (mincore_vec[page_index]&1) {
             printf("%lu ", (unsigned long)page_index);
+            n_cached++;
         }
     }
-    printf("\n");
+    printf("\nSummary: %u of %u pages (%.2lf%%) cached.\n", n_cached, n_pages, (double)n_cached*100/n_pages );
     free(mincore_vec);
     munmap(file_mmap, file_stat.st_size);
     close(fd);
